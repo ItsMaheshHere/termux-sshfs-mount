c654b19b3065ee3a272d9326a7029cea3d082f99
MAH3SH <maheshsaran477@gmail.com>
2025-12-28 07:55:08 +0530

    Shell Script file created

---
+++ connect_phone.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 1.Ask username
+echo "Enter your Termux username(type 'whoami' in termux to see):"
+read -p "Username:" username
+
+# 2.Get IP
+IP=$(ip route show default | awk '{print $3}')
+if[-z "$IP"]; then
+	echo "ERROR: Could Not Find IP"
+	exit 1
+fi
+
+echo "Target: $username@$IP"
+
+# 3.Mount Storage
+mkdir -p ~/AndroidMount
+sshfs -p 8022 $username@IP:storage/emulated/0 ~/AndroidMount
+
+echo "Success! Storage mounted "
+
